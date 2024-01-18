0039757e65f27ebbf714c7f973b59eb4042c9472
kithmina1999 <kithgaming99@gmail.com>
2024-01-18 13:14:09 +0530

    setting up pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,26 @@
-
+import { BrowserRouter, Routes , Route } from 'react-router-dom'
 import './App.css'
+import Signin from './pages/Signin'
+import About from './pages/About'
+import Home from './pages/Home'
+import Dashboard from './pages/Dashboard'
+import Projects from './pages/Projects'
+import Signup from './pages/Signup'
 
 function App() {
 
   return (
-    <div className='text-3xl text-red-500'>My blog</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/sign-in' element={<Signin />} />
+        <Route path='/sign-up' element={<Signup />} />
+        <Route path='/projects' element={<Projects />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
+    
   )
 }
 
